eb1be8d3ed1a88d63517ad97b5b4e33db3fc650e
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2023-09-02 11:32:38 +0300

    Done!

--- WorkInJS/js/main.js
+++ WorkInJS/js/main.js
@@ -1,1 +1,44 @@
-console.log(3);
+function counter() {
+    let count = 0;
+
+    function inner(value) {
+        count += value;
+
+        console.log(count);
+    };
+
+    return inner;
+}
+
+let fn = counter();
+
+fn(3);      //3
+fn(5);      //8
+fn(228);    //236
+
+//------------------------------------
+
+function getUpdatedArr(value) {
+    let array = [];
+
+    
+
+    function inner(value) {
+        if (value == undefined) {
+            return array = [];
+        };
+
+        array.push(value);
+
+        console.log(array);
+    };
+
+    return inner;
+}
+
+let fn2 = getUpdatedArr();
+
+fn2(4);
+fn2(5);
+fn2();
+fn2({name: "name"})
